2562d2d81f43e598ae85c962e59ba934f2c80398
taytestokes <taytestokes9328@gmail.com>
2021-12-22 14:04:21 -0700

    Updates home landing page with introduction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,9 @@
 import React from 'react'
-import { FiGithub, FiTwitter, FiLinkedin, FiCoffee } from 'react-icons/fi'
 
 export const Footer = () => {
-  const githubLink = 'https://github.com/taytestokes'
-  const twitterLink = 'https://twitter.com/taytestokes'
-  const linkedinLink = 'https://linkedin.com/taytestokes'
-  const kofiLink = 'https://ko-fi.com/taytestokes'
-
   return (
-    <div className="container mt-auto mx-auto py-8 px-4 flex items-center justify-center text-gray-400 dark:text-gray-700">
-      <ul className="flex items-center">
-        <li className="flex items-center p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
-          <a href={kofiLink} rel="noopener noreferrer" target="_blank">
-            <FiCoffee />
-          </a>
-        </li>
-
-        <li className="flex items-center p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
-          <a href={linkedinLink} rel="noopener noreferrer" target="_blank">
-            <FiLinkedin />
-          </a>
-        </li>
-
-        <li className="flex items-center p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
-          <a href={twitterLink} rel="noopener noreferrer" target="_blank">
-            <FiTwitter />
-          </a>
-        </li>
-
-        <li className="flex items-center p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
-          <a href={githubLink} rel="noopener noreferrer" target="_blank">
-            <FiGithub />
-          </a>
-        </li>
-      </ul>
+    <div className="container mx-auto py-8 px-2 flex flex-col items-center text-gray-400 dark:text-gray-700">
+      <span>&copy; Tayte Stokes - 2022</span>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
+import { FiGithub, FiTwitter, FiLinkedin, FiCoffee } from 'react-icons/fi'
 
 import { Layout } from '../components/Layout'
 
-import profilePhoto from '..//public/images/profile.jpeg'
+import profilePhoto from '../public/images/profile.jpeg'
 
 export default function Home() {
   return (
     <Layout>
-      <div className="w-full flex flex-col flex-grow items-center justify-center">
+      <div className="w-full flex flex-col flex-grow items-center justify-center text-center p-8">
         <div className="flex items-center rounded-full border-4 border-gray-200 dark:border-gray-800 ">
           <Image
             alt="Profile photo of Tayte Stokes"
@@ -18,8 +19,34 @@ export default function Home() {
           />
         </div>
 
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100 mt-4">Tayte Stokes</h1>
-        <p className="text-gray-800 dark:text-gray-400">Software Engineer</p>
+        <h1 className="font-bold text-4xl mt-4">Tayte Stokes</h1>
+
+        <p className="mt-2 text-gray-600 dark:text-gray-400">
+          I'm a digital craftsmen focusing on web development. I'm currently employed and work full
+          time as a Software Engineer at Podium.
+        </p>
+
+        <div className="w-full flex items-center justify-center text-sm mt-8 space-x-4 text-gray-600 dark:text-gray-400">
+          <a
+            className="flex items-center p-4 rounded-md bg-gray-100 dark:bg-gray-900 leading-none"
+            href="https://github.com/taytestokes"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FiGithub className="mr-2" />
+            Github
+          </a>
+
+          <a
+            className="flex items-center p-4 rounded-md bg-gray-100 dark:bg-gray-900 leading-none"
+            href="https://twitter.com/taytestokes"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FiTwitter className="mr-2" />
+            Twitter
+          </a>
+        </div>
       </div>
     </Layout>
   )
