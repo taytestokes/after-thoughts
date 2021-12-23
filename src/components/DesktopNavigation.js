7f81d57357d79d835acf635fe5866a3c04ff14ee
taytestokes <taytestokes9328@gmail.com>
2021-12-23 13:05:55 -0700

    Removes home link on desktop views and use logo to navigate to home

--- src/components/DesktopNavigation.js
+++ src/components/DesktopNavigation.js
@@ -9,17 +9,6 @@ export const DesktopNavigation = () => {
 
   return (
     <div className="hidden md:flex items-center ml-auto">
-      <Link href="/">
-        <a
-          className={`px-4 py-3 leading-none ${
-            router.pathname === '/'
-              ? 'text-gray-900 dark:text-gray-100'
-              : 'text-gray-600 dark:text-gray-400'
-          }  rounded-md hover:bg-gray-100 dark:hover:bg-gray-900`}
-        >
-          Home
-        </a>
-      </Link>
       <Link href="/blog">
         <a
           className={`px-4 py-3 leading-none ${
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Link from 'next/link'
+
 import { LogoIcon } from './LogoIcon'
 import { MobileNavigation } from './MobileNavigation'
 import { DesktopNavigation } from './DesktopNavigation'
@@ -7,9 +9,11 @@ import { DesktopNavigation } from './DesktopNavigation'
 export const Header = () => {
   return (
     <header className="relative w-full flex items-center justify-between p-8">
-      <div className="px-4">
-        <LogoIcon />
-      </div>
+      <Link href="/">
+        <a className="px-4">
+          <LogoIcon />
+        </a>
+      </Link>
 
       {/* Mobile Navigation */}
       <MobileNavigation />
