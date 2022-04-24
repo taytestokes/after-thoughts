94280175dbc5cbd8a6b802896120a2d615d6d4be
taytestokes <taytestokes9328@gmail.com>
2022-04-24 16:04:49 -0600

    Removes nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,9 @@
-import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import nprogress from 'nprogress'
 
 import '../styles/main.css'
-import '../styles/nprogress.css'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', nprogress.start)
-    router.events.on('routeChangeComplete', nprogress.done)
-    router.events.on('routeChangeError', nprogress.done)
-  }, [router])
 
   return <Component {...pageProps} />
 }
