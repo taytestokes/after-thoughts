26ac5a10008e6ad3122ba3e4e5d53d998e46b407
taytestokes <taytestokes9328@gmail.com>
2022-04-24 16:05:19 -0600

    Removes unused variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
-import { useRouter } from 'next/router'
-
 import '../styles/main.css'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
   return <Component {...pageProps} />
 }
 
