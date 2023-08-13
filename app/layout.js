c146341de37f5fe8e81390e59a382bea8b21c578
taytestokes <taytestokes9328@gmail.com>
2023-08-13 13:40:50 -0600

    Migrates to NextJs 13 from 12

---
+++ app/layout.js
@@ -0,0 +1,68 @@
+import Link from 'next/link'
+import { BrainIcon } from '../components/BrainIcon'
+
+// Tailwind Styles
+import './globals.css'
+
+// Generate Base Metadata For Entire Application
+export const metadata = {
+  metaDatabase: new URL('https://afterthoughts.dev'),
+  title: {
+    default: 'After Thoughts',
+    template: 'After Thoughts | %s',
+  },
+  description: 'A software development blog created by Tayte Stokes',
+  keywords: ['JavaScript', 'React', 'Elixir', 'Phoenix'],
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  openGraph: {
+    title: 'After Thoughts',
+    description: 'A software development blog created by Tayte Stokes',
+    defaultUrl: 'https://afterthoughts.dev',
+    siteName: 'After Thoughts',
+    locale: 'en-US',
+    type: 'website',
+    image: '/after-thoughts-block-logo.jpg',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'After Thoughts',
+    description: 'A software development blog created by Tayte Stokes',
+    creator: '@taytestokes',
+    images: ['/after-thoughts-block-logo.jpg'],
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white">
+        <div className="w-screen min-h-screen flex flex-col">
+          <header className="container flex items-center mx-auto py-8 px-4">
+            <Link href="/">
+              <BrainIcon size={45} />
+            </Link>
+          </header>
+
+          <main className="flex flex-col grow">
+            {typeof children === 'function' ? children() : children}
+          </main>
+        </div>
+      </body>
+    </html>
+  )
+}
--- components/PostCard.js
+++ components/PostCard.js
@@ -6,8 +6,7 @@ export const PostCard = ({ post }) => {
   const publishedDateDistance = format(new Date(post.data.publishedAt), 'MMMM dd, yyyy')
 
   return (
-    <Link href={`/${post.slug}`}>
-      <a className="flex flex-col lg:flex-row items-start mb-12">
+    <Link href={`/${post.slug}`} className="flex flex-col lg:flex-row items-start mb-12">
         <div className="w-full lg:w-1/4 text-neutral-300">
           <span>{publishedDateDistance}</span>
         </div>
@@ -18,7 +17,6 @@ export const PostCard = ({ post }) => {
           </h2>
           <span className="text-neutral-300 mt-4">{post.data.excerpt}</span>
         </div>
-      </a>
     </Link>
   )
 }
