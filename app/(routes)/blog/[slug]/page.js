f430aa4ec2b8c872ff2b4e9b57390c6e90e26a45
taytestokes <taytestokes9328@gmail.com>
2023-09-06 16:43:55 -0600

    Upgrade to remote-mdx

--- app/(routes)/blog/[slug]/page.js
+++ app/(routes)/blog/[slug]/page.js
@@ -1,8 +1,6 @@
-import { marked } from 'marked'
-
 import { TableOfContents } from '../_components/TableOfContents'
 
-import { getPostSlugs, getPostBySlug } from '../../../../utils/blog'
+import { getPostSlugs, getPostBySlug } from '../../../_utils/blog'
 
 /**
  * Generate each static blog post page for each
@@ -12,7 +10,7 @@ export async function generateStaticParams() {
   const postSlugs = getPostSlugs()
   const paths = postSlugs.map((slug) => {
     return {
-      slug: slug.replace('.md', ''),
+      slug: slug.replace('.mdx', ''),
     }
   })
 
@@ -23,31 +21,30 @@ export async function generateStaticParams() {
  * Get the blog post from the database for the current static blog post page
  * using it's slug value.
  */
-function getPostData(slug) {
-  const post = getPostBySlug(slug)
+async function getPostData(slug) {
+  const post = await getPostBySlug(slug)
 
   return {
-    postContent: post.content,
-    postData: post.data,
+    content: post.content,
+    data: post.frontmatter,
   }
 }
 
-export default function PostPage({ params: { slug } }) {
-  const { postContent, postData } = getPostData(slug)
+export default async function PostPage({ params: { slug } }) {
+  const { content, data } = await getPostData(slug)
 
   return (
     <article className="container flex flex-col mx-auto px-4 py-8">
       <div className="lg:p-12 rounded-md text-center">
-        <h1 className="text-3xl font-bold">{postData.title}</h1>
+        <h1 className="text-3xl font-bold">{data?.title}</h1>
       </div>
       <div className="w-full flex mt-8 gap-8">
         <div className="hidden lg:flex min-w-[250px] max-w-[250px]">
           <TableOfContents />
         </div>
-        <div
-          className="w-1/2 flex flex-col flex-1 break-words prose prose-invert prose-pre:bg-zinc-800 prose-img:rounded-md prose-img:border-4 prose-img:border-zinc-600"
-          dangerouslySetInnerHTML={{ __html: marked(postContent) }}
-        />
+        <section className="w-1/2 flex flex-col flex-1 break-words prose prose-invert prose-pre:bg-zinc-800 prose-img:rounded-md prose-img:border-4 prose-img:border-zinc-600">
+          {content}
+        </section>
       </div>
     </article>
   )
---
+++ app/_utils/blog.js
@@ -0,0 +1,50 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+import { compileMDX } from 'next-mdx-remote/rsc'
+import rehypeSlug from 'rehype-slug'
+
+/**
+ * Get a collection of all blog post file
+ * names
+ */
+export const getPostSlugs = () => {
+  return fs.readdirSync(path.join('app', '_content'))
+}
+
+/**
+ * Get a collection of all blog posts in an
+ * object form after reading the file contents
+ */
+export const getPosts = async (postSlugs) => {
+  return postSlugs.map((slug) => {
+    const { data } = matter(fs.readFileSync(path.join('app', '_content', slug), 'utf-8'))
+
+    return {
+      slug: slug.replace('.mdx', ''),
+      data,
+    }
+  })
+}
+
+/**
+ * Get a specific blog post contents by using
+ * the post slug
+ */
+export const getPostBySlug = async (slug) => {
+  const { frontmatter, content } = await compileMDX({
+    source: fs.readFileSync(path.join('app', '_content', `${slug}.mdx`), 'utf-8'),
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        rehypePlugins: [rehypeSlug],
+      },
+    },
+  })
+
+  return {
+    content,
+    frontmatter,
+  }
+}
