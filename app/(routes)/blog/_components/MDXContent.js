cae81a23a75b9866bdb731261a6f113bf93115e3
taytestokes <taytestokes9328@gmail.com>
2023-09-06 20:51:06 -0600

    Inverts prose for mdx content

--- app/(routes)/blog/_components/MDXContent.js
+++ app/(routes)/blog/_components/MDXContent.js
@@ -5,7 +5,7 @@ import 'highlight.js/styles/github-dark.css'
 
 export function MDXContent({ content }) {
   return (
-    <section className="w-1/2 flex flex-col flex-1 break-words prose prose-pre:p-0 prose-img:rounded-md prose-img:border-4 prose-img:border-zinc-600">
+    <section className="w-1/2 flex flex-col flex-1 break-words prose prose-invert prose-pre:p-0 prose-img:rounded-md prose-img:border-4 prose-img:border-zinc-600">
       {content}
     </section>
   )
