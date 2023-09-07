2423e1eb521732f154fc1f01b19bc888cfb823a7
taytestokes <taytestokes9328@gmail.com>
2023-09-06 19:47:31 -0600

    Adds highlighting

---
+++ app/(routes)/blog/_components/MDXContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Code highlight styles
+import 'highlight.js/styles/github-dark.css'
+
+export function MDXContent({ content }) {
+  return (
+    <section className="w-1/2 flex flex-col flex-1 break-words prose prose-pre:p-0 prose-img:rounded-md prose-img:border-4 prose-img:border-zinc-600">
+      {content}
+    </section>
+  )
+}
